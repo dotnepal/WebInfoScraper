d62e40081bc6adcdb5a578a0ecb50d295d5bd7c2
Samundra Shrestha <samundra.shr@gmail.com>
2017-08-02 12:58:15 +0700

    remove SG API key

--- framework/text_scrapper/helpers/send_email.py
+++ framework/text_scrapper/helpers/send_email.py
@@ -3,7 +3,7 @@ import sendgrid
 class SendgridEmailHelper:
     @staticmethod
     def send_email(scrapper_name, to_email, subject):
-        sg = sendgrid.SendGridClient('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')
+        sg = sendgrid.SendGridClient('SENDGRID_API_KEY_HERE')
         message = sendgrid.Mail()
         message.add_to(to_email)
         message.set_subject(subject)
